9457b09ba1cd77767a15f7332e69f1767b8bfd7a
Anojan1520 <unicomtic1520@gmail.com>
2024-11-10 20:22:36 +0530

    Add Objects in CourseDto

--- MS3-Back-End/DTOs/RequestDTOs/CourseRequestDTO.cs
+++ MS3-Back-End/DTOs/RequestDTOs/CourseRequestDTO.cs
@@ -2,6 +2,13 @@
 {
     public class CourseRequestDTO
     {
+        public int CategoryId { get; set; }
+        public string CourseName { get; set; } = string.Empty;
+        public int Level { get; set; }
+        public decimal CourseFee { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Prerequisites { get; set; } = string.Empty;
+        public string ImagePath { get; set; } = string.Empty;
 
     }
 }
