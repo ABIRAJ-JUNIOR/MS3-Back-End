ca4ca83c71cf85cd09a41d03a742c7306ce5f9ac
Anojan1520 <unicomtic1520@gmail.com>
2024-11-10 20:58:27 +0530

    implement Icourse Repository methods

--- MS3-Back-End/IRepository/ICourseRepository.cs
+++ MS3-Back-End/IRepository/ICourseRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace MS3_Back_End.IRepository
+﻿using MS3_Back_End.Entities;
+
+namespace MS3_Back_End.IRepository
 {
     public interface ICourseRepository
     {
-
+        Task<Course> AddCourse(Course courseReq);
+        Task<List<Course>> GetAllCourse();
+        Task<Course> GetCourseById(Guid CourseId);
+        Task<Course> UpdateCourse(Course course);
+        Task<string> DeleteCourse(Course course);
     }
 }
