566971a806dcd5a839d40c2a136b5722453fcbe8
Anojan1520 <unicomtic1520@gmail.com>
2024-11-10 20:29:22 +0530

    inject db in Course repo

--- MS3-Back-End/Repository/CourseRepositoy.cs
+++ MS3-Back-End/Repository/CourseRepositoy.cs
@@ -1,6 +1,16 @@
-﻿namespace MS3_Back_End.Repository
+﻿using MS3_Back_End.DBContext;
+
+namespace MS3_Back_End.Repository
 {
     public class CourseRepositoy
     {
+        private readonly AppDBContext _Db;
+        public CourseRepositoy(AppDBContext db)
+        {
+            _Db = db;
+            
+        }
+
+       
     }
 }
