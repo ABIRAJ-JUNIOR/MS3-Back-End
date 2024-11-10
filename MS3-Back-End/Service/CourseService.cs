e1dcfeddd94b86c1fd554f0b6a931abd1aa79ad6
Anojan1520 <unicomtic1520@gmail.com>
2024-11-10 21:40:15 +0530

    implement search in Service

--- MS3-Back-End/Service/CourseService.cs
+++ MS3-Back-End/Service/CourseService.cs
@@ -10,46 +10,79 @@ namespace MS3_Back_End.Service
         private readonly ICourseRepository _courseRepository;
         public CourseService(ICourseRepository courseRepository)
         {
-             _courseRepository = courseRepository;
+            _courseRepository = courseRepository;
         }
 
 
-        async Task<CourseResponseDTO> AddCourse(CourseRequestDTO courseReq)
+        public async Task<CourseResponseDTO> AddCourse(CourseRequestDTO courseReq)
         {
-            
-                var obj = new Course
-                {
-                    CategoryId = courseReq.CategoryId,
-                    CourseName = courseReq.CourseName,
-                    Level = courseReq.Level,
-                    CourseFee = courseReq.CourseFee,
-                    Description = courseReq.Description,
-                    Prerequisites = courseReq.Prerequisites,
-                    ImagePath = courseReq.ImagePath,
-                    CreatedDate = DateTime.Now,
-                    UpdatedDate = DateTime.Now
-                };
 
-                var data = await _courseRepository.AddCourse(obj);
+            var Course = new Course
+            {
+                CategoryId = courseReq.CategoryId,
+                CourseName = courseReq.CourseName,
+                Level = courseReq.Level,
+                CourseFee = courseReq.CourseFee,
+                Description = courseReq.Description,
+                Prerequisites = courseReq.Prerequisites,
+                ImagePath = courseReq.ImagePath,
+                CreatedDate = DateTime.Now,
+                UpdatedDate = DateTime.Now
+            };
 
-                var ReturnObj = new CourseResponseDTO
-                {
-                    CategoryId = data.CategoryId,
-                    CourseName = data.CourseName,
-                    Level = data.Level,
-                    CourseFee = data.CourseFee,
-                    Description = data.Description,
-                    Prerequisites = data.Prerequisites,
-                    ImagePath = data.ImagePath,
-                    CreatedDate = data.CreatedDate,
-                    UpdatedDate = data.UpdatedDate
-                };
-           
+            var data = await _courseRepository.AddCourse(Course);
+
+            var CourseResponse = new CourseResponseDTO
+            {
+                CategoryId = data.CategoryId,
+                CourseName = data.CourseName,
+                Level = data.Level,
+                CourseFee = data.CourseFee,
+                Description = data.Description,
+                Prerequisites = data.Prerequisites,
+                ImagePath = data.ImagePath,
+                CreatedDate = data.CreatedDate,
+                UpdatedDate = data.UpdatedDate
+            };
+
+            return CourseResponse;
+
         }
 
+        public async Task<List<CourseResponseDTO>> SearchCourse(string SearchText)
+        {
+            var data = await _courseRepository.SearchCourse(SearchText);
+            if (data == null)
+            {
+                throw new Exception("Search Not Found");
+            }
 
+            var CourseResponse = new List<CourseResponseDTO>();
+            foreach (var item in data)
+            {
+                var obj = new CourseResponseDTO
+                {
+                    Id = item.Id,
+                    CategoryId = item.CategoryId,
+                    CourseName = item.CourseName,
+                    Level = item.Level,
+                    CourseFee = item.CourseFee,
+                    Description = item.Description,
+                    Prerequisites = item.Prerequisites,
+                    ImagePath = item.ImagePath,
+                    CreatedDate = item.CreatedDate,
+                    UpdatedDate = item.UpdatedDate
+                };
+                CourseResponse.Add(obj);
 
+            }
+            return CourseResponse;
+
+        }
 
 
     }
+
+
+
 }
