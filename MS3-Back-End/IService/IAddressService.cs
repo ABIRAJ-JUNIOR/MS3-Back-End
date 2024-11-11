c40a1254883ff7f1a44f2013c2acbc3059009979
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-11 16:57:03 +0530

    Iservice Addmethod done

--- MS3-Back-End/IService/IAddressService.cs
+++ MS3-Back-End/IService/IAddressService.cs
@@ -1,6 +1,11 @@
-﻿namespace MS3_Back_End.IService
+﻿using MS3_Back_End.DTOs.RequestDTOs.Address;
+using MS3_Back_End.DTOs.ResponseDTOs.Address;
+
+namespace MS3_Back_End.IService
 {
     public interface IAddressService
     {
+        Task<AddressResponseDTO> AddAddress(AddressRequestDTO address);
+
     }
 }
