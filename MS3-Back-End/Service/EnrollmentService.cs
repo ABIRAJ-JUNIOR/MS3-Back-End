612dd93765abdf35af507a2398863e72b6a66dbf
Anojan1520 <unicomtic1520@gmail.com>
2024-11-11 23:02:28 +0530

    add dependancy injection in enrollment Services

--- MS3-Back-End/Service/EnrollmentService.cs
+++ MS3-Back-End/Service/EnrollmentService.cs
@@ -1,7 +1,18 @@
-﻿namespace MS3_Back_End.Service
+﻿using MS3_Back_End.IRepository;
+
+namespace MS3_Back_End.Service
 {
     public class EnrollmentService
     {
+        private readonly IEnrollmentRepository _enrollmentRepository;
+        public EnrollmentService(IEnrollmentRepository enrollmentRepository)
+        {
+            _enrollmentRepository = enrollmentRepository;
+        }
+
+
+
+
 
     }
 }
