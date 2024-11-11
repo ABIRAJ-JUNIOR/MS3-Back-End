d3178f05972586d151fb9defe0662c0b5fa62bec
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-11-11 22:11:05 +0530

    implement create Assessment Method in repo

--- MS3-Back-End/Repository/AssessmentRepository.cs
+++ MS3-Back-End/Repository/AssessmentRepository.cs
@@ -1,4 +1,5 @@
 ﻿using MS3_Back_End.DBContext;
+using MS3_Back_End.Entities;
 using MS3_Back_End.IRepository;
 
 namespace MS3_Back_End.Repository
@@ -10,6 +11,13 @@ namespace MS3_Back_End.Repository
         public AssessmentRepository(AppDBContext dbContext)
         {
             _dbContext = dbContext;
+        }
+
+        public async Task<Assessment> AddAssessment(Assessment assessment)
+        {
+            var assessmentData = await _dbContext.Assessments.AddAsync(assessment);
+            await _dbContext.SaveChangesAsync();
+            return assessmentData.Entity;
         }
     }
 }
