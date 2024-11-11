9d6b43727d6a0eb62bd9b49c0f1afb28372f4f08
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-11 16:42:00 +0530

    service dependency inject

--- MS3-Back-End/Service/AddressServise.cs
+++ MS3-Back-End/Service/AddressServise.cs
@@ -1,6 +1,15 @@
-﻿namespace MS3_Back_End.Service
+﻿using MS3_Back_End.IRepository;
+using MS3_Back_End.IService;
+
+namespace MS3_Back_End.Service
 {
-    public class AddressServise
+    public class AddressServise: IAddressService
     {
+        private readonly IAddressRepository _addressRepository;
+
+        public AddressServise(IAddressRepository addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
     }
 }
