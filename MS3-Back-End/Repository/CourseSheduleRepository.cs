c0b4c17fdd66a6441f12618f9bfb410effc7f29f
Anojan1520 <unicomtic1520@gmail.com>
2024-11-11 18:50:09 +0530

    dependancy inject Course Repo

--- MS3-Back-End/Repository/CourseSheduleRepository.cs
+++ MS3-Back-End/Repository/CourseSheduleRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace MS3_Back_End.Repository
+﻿using Microsoft.EntityFrameworkCore;
+using MS3_Back_End.DBContext;
+using MS3_Back_End.Entities;
+
+namespace MS3_Back_End.Repository
 {
     public class CourseSheduleRepository
     {
+        private readonly AppDBContext _Db;
+        public CourseSheduleRepository(AppDBContext db)
+        {
+            _Db = db;
+
+        }
+
+      
     }
 }
