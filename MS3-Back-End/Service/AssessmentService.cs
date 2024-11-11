3f5aec4334e4b76ade88575a8d1432035889274e
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-11-11 22:03:54 +0530

    implement Assessment Service injection

--- MS3-Back-End/Service/AssessmentService.cs
+++ MS3-Back-End/Service/AssessmentService.cs
@@ -1,6 +1,15 @@
-﻿namespace MS3_Back_End.Service
+﻿using MS3_Back_End.IRepository;
+using MS3_Back_End.IService;
+
+namespace MS3_Back_End.Service
 {
-    public class AssessmentService
+    public class AssessmentService : IAssessmentService
     {
+        private readonly IAssessmentRepository _repository;
+
+        public AssessmentService(IAssessmentRepository repository)
+        {
+            _repository = repository;
+        }
     }
 }
