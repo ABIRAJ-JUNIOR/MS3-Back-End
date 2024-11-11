7136696d80cb4f5532111d53c0bb19b7630d65ee
Anojan1520 <unicomtic1520@gmail.com>
2024-11-11 18:34:21 +0530

    Create Course Repository Folder

---
+++ MS3-Back-End/DTOs/RequestDTOs/Course/UpdateCourseSheduleDTO.cs
@@ -0,0 +1,19 @@
+﻿using MS3_Back_End.Entities;
+
+namespace MS3_Back_End.DTOs.RequestDTOs.Course
+{
+    public class UpdateCourseSheduleDTO
+    {
+        public Guid? Id { get; set; }
+        public Guid? CourseId { get; private set; }
+        public DateTime? StartDate { get; set; } = DateTime.MinValue;
+        public DateTime? EndDate { get; set; } = DateTime.MinValue;
+        public int? Duration { get; set; }
+        public string? Time { get; set; } = string.Empty;
+        public string? Location { get; set; } = string.Empty;
+        public int? MaxStudents { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public ScheduleStatus? ScheduleStatus { get; set; }
+
+    }
+}
---
+++ MS3-Back-End/DTOs/ResponseDTOs/Course/CourseSheduleResponseDTO.cs
@@ -0,0 +1,20 @@
+﻿using MS3_Back_End.Entities;
+
+namespace MS3_Back_End.DTOs.ResponseDTOs.Course
+{
+    public class CourseSheduleResponseDTO
+    {
+        public Guid Id { get; set; }
+        public Guid CourseId { get; private set; }
+        public DateTime StartDate { get; set; } = DateTime.MinValue;
+        public DateTime EndDate { get; set; } = DateTime.MinValue;
+        public int Duration { get; set; }
+        public string Time { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public int MaxStudents { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public ScheduleStatus ScheduleStatus { get; set; }
+
+    }
+}
