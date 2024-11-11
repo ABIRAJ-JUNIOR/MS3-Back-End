5486297f0217c4835e0e7682481f8b158b4fc19b
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-11 16:13:36 +0530

    repository dependency inject

--- MS3-Back-End/Repository/AddressRepository.cs
+++ MS3-Back-End/Repository/AddressRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace MS3_Back_End.Repository
+﻿using MS3_Back_End.DBContext;
+using MS3_Back_End.IRepository;
+
+namespace MS3_Back_End.Repository
 {
-    public class AddressRepository
+    public class AddressRepository: IAddressRepository
     {
+        private readonly AppDBContext _dbContext;
+
+        public AddressRepository(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        
     }
 }
