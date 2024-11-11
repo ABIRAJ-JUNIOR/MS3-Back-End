1b6040f9c4d517481d74d4ac176035355682684b
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-11-11 22:02:59 +0530

    implement Assessment Repo injection

---
+++ MS3-Back-End/Repository/AssessmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using MS3_Back_End.DBContext;
+using MS3_Back_End.IRepository;
+
+namespace MS3_Back_End.Repository
+{
+    public class AssessmentRepository : IAssessmentRepository
+    {
+        private readonly AppDBContext _dbContext;
+
+        public AssessmentRepository(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
