7ca4fec0b257cdaf7f44f7142438bb6c5c59865f
Anojan1520 <unicomtic1520@gmail.com>
2024-11-11 22:33:56 +0530

    Implement Dependanncy in enrollment Repo

--- MS3-Back-End/Repository/EnrollmentRepository.cs
+++ MS3-Back-End/Repository/EnrollmentRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace MS3_Back_End.Repository
+﻿using MS3_Back_End.DBContext;
+
+namespace MS3_Back_End.Repository
 {
     public class EnrollmentRepository
     {
+        private readonly AppDBContext _Db;
+        public EnrollmentRepository(AppDBContext db)
+        {
+            _Db = db;
+
+        }
+
+
     }
 }
