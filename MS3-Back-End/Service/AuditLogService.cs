248faf9c2c1f37176a9b9d5382125f4a361ee309
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-12 13:29:14 +0530

    Audit log service done

--- MS3-Back-End/Service/AuditLogService.cs
+++ MS3-Back-End/Service/AuditLogService.cs
@@ -1,14 +1,45 @@
-﻿using MS3_Back_End.IService;
+﻿using MS3_Back_End.DTOs.RequestDTOs.AuditLog;
+using MS3_Back_End.DTOs.ResponseDTOs.AuditLog;
+using MS3_Back_End.Entities;
+using MS3_Back_End.IRepository;
+using MS3_Back_End.IService;
+using MS3_Back_End.Repository;
 
 namespace MS3_Back_End.Service
 {
     public class AuditLogService: IAuditLogService
     {
-        private readonly IAuditLogService _Auditservice;
+        private readonly IAuditLogRepository _auditLogRepository;
 
-        public AuditLogService(IAuditLogService auditservice)
+        public AuditLogService(IAuditLogRepository auditLogRepository)
         {
-            _Auditservice = auditservice;
+            _auditLogRepository = auditLogRepository;
         }
+
+        public async Task<AuditLogResponceDTO> AddAuditLog(AuditLogRequestDTO auditLog)
+        {
+
+            var AuditLog=new AuditLog()
+            {
+                Action= auditLog.Action,
+                Details= auditLog.Details,
+                ActionDate= auditLog.ActionDate,
+                AdminId= auditLog.AdminId,
+            };
+
+            var data = await _auditLogRepository.AddAuditLog(AuditLog);
+
+            var returndata = new AuditLogResponceDTO()
+            {
+                AdminId = data.AdminId,
+                ActionDate = data.ActionDate,
+                Details = data.Details,
+                Id = data.Id,
+                Action = auditLog.Action,
+            };
+
+            return returndata;
+        }
+
     }
 }
