aa89e599ae94e83d6198775037f7ff1a41ea5cc7
thushag <141636675+thushag@users.noreply.github.com>
2024-11-13 00:42:09 +0530

    ContactUs Interface Created

--- MS3-Back-End/IService/IContactUsService.cs
+++ MS3-Back-End/IService/IContactUsService.cs
@@ -1,6 +1,11 @@
-﻿namespace MS3_Back_End.IService
+﻿using MS3_Back_End.DTOs.RequestDTOs.ContactUs;
+using MS3_Back_End.DTOs.ResponseDTOs.ContactUs;
+
+namespace MS3_Back_End.IService
 {
     public interface IContactUsService
     {
+        Task<ContactUsResponseDTO> AddMessage(ContactUsRequestDTO requestDTO);
+
     }
 }
