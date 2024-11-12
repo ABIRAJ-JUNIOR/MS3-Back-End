764fc73816838de3a3a40a2f7b9785d44e5c0110
Arshad-85 <mohammedarshath339@gmail.com>
2024-11-13 01:38:21 +0530

    GetCourseCategoryById Repo Interface

--- MS3-Back-End/IRepository/ICourseCategoryRepository.cs
+++ MS3-Back-End/IRepository/ICourseCategoryRepository.cs
@@ -5,6 +5,7 @@ namespace MS3_Back_End.IRepository
     public interface ICourseCategoryRepository
     {
         Task<CourseCategory> AddCategory(CourseCategory categoryReq);
+        Task<CourseCategory> GetCourseCategoryById(Guid Id);
 
     }
 }
