87a869e24108e903b1f9179b6e806f1b01f36810
UT01520 <UT01520@UNICOMTIC15>
2024-11-12 15:06:54 +0530

    Add Depedancy injection

--- MS3-Back-End/Repository/AnnouncementRepository.cs
+++ MS3-Back-End/Repository/AnnouncementRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace MS3_Back_End.Repository
+﻿using MS3_Back_End.DBContext;
+
+namespace MS3_Back_End.Repository
 {
     public class AnnouncementRepository
     {
+        private readonly AppDBContext _Db;
+        public AnnouncementRepository(AppDBContext db)
+        {
+            _Db = db;
+        }
     }
 }
