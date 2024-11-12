ee78ca145c7d27d299fe48e7a127945a6a6f068c
Arshad-85 <mohammedarshath339@gmail.com>
2024-11-13 01:01:51 +0530

    GetAllMessages Service

--- MS3-Back-End/Service/ContactUsService.cs
+++ MS3-Back-End/Service/ContactUsService.cs
@@ -38,5 +38,28 @@ namespace MS3_Back_End.Service
             return newContactUs;
         }
 
+        public async Task<List<ContactUsResponseDTO>> GetAllMessages()
+        {
+            var allMessages = await _contactUsRepository.GetAllMessages();
+            if (allMessages == null)
+            {
+                throw new Exception("No messages");
+            }
+            var ContactUsResponse = new List<ContactUsResponseDTO>();
+            foreach (var message in allMessages)
+            {
+                var obj = new ContactUsResponseDTO
+                {
+                    Id = message.Id,
+                    Name = message.Name,
+                    Email = message.Email,
+                    Message = message.Message,
+                };
+                ContactUsResponse.Add(obj);
+            }
+            return ContactUsResponse;
+        }
+
+
     }
 }
