659528c1daf47a5cd3ed71642249f260336ea9b9
Arshad-85 <mohammedarshath339@gmail.com>
2024-11-13 01:27:38 +0530

    CourseCategoryRepository dependency injection created

--- MS3-Back-End/Repository/CourseCategoryRepository.cs
+++ MS3-Back-End/Repository/CourseCategoryRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace MS3_Back_End.Repository
+﻿using MS3_Back_End.DBContext;
+using MS3_Back_End.IRepository;
+
+namespace MS3_Back_End.Repository
 {
-    public class CourseCategoryRepository
+    public class CourseCategoryRepository: ICourseCategoryRepository
     {
+        private readonly AppDBContext _appDBContext;
+
+        public CourseCategoryRepository(AppDBContext appDBContext)
+        {
+            _appDBContext = appDBContext;
+        }
+
     }
 }
