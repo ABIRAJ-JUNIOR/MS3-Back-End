c0ac835e43d3e53eaa4bda025b1bb7c859f30ed2
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-12 13:51:56 +0530

    Irepo getallaudit

--- MS3-Back-End/IRepository/IAuditLogRepository.cs
+++ MS3-Back-End/IRepository/IAuditLogRepository.cs
@@ -6,6 +6,8 @@ namespace MS3_Back_End.IRepository
 
     {
         Task<AuditLog> AddAuditLog(AuditLog auditLog);
+        Task<List<AuditLog>> GetAllAuditlogs(List<AuditLog> auditLogs);
+
 
     }
 }
