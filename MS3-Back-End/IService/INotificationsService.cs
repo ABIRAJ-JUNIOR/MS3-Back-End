cf006610b6846913a8ddcbfebc8545b41c2df839
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-13 01:37:33 +0530

    INotificationsService Addnotification

--- MS3-Back-End/IService/INotificationsService.cs
+++ MS3-Back-End/IService/INotificationsService.cs
@@ -1,6 +1,12 @@
-﻿namespace MS3_Back_End.IService
+﻿using MS3_Back_End.DTOs.RequestDTOs.Notification;
+using MS3_Back_End.DTOs.ResponseDTOs.Notification;
+
+namespace MS3_Back_End.IService
 {
     public interface INotificationsService
+
     {
+         Task<NotificationResponceDTO> AddNotification(NotificationRequestDTO notificationDTO);
+
     }
 }
