2b1c4db2537863ab853cf22f599305a85176c252
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-13 03:47:58 +0530

    updatenotification repo done

--- MS3-Back-End/Repository/NotificationRepository.cs
+++ MS3-Back-End/Repository/NotificationRepository.cs
@@ -2,6 +2,7 @@
 using MS3_Back_End.DBContext;
 using MS3_Back_End.Entities;
 using MS3_Back_End.IRepository;
+using System.Runtime.InteropServices;
 
 namespace MS3_Back_End.Repository
 {
@@ -30,6 +31,12 @@ namespace MS3_Back_End.Repository
 
             var data =await _dbContext.Notifications.SingleOrDefaultAsync(a => a.Id == Id);
             return data;
+        }
+        public async Task<Notification> updatenotification(Notification notification)
+        {
+             var data= _dbContext.Notifications.Update(notification);
+            _dbContext.SaveChanges();
+            return notification;
         }
     }
 }
