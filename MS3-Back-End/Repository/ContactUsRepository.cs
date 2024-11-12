fcd97ece1c5e86042c6402be1168a61b8e1663b0
thushag <141636675+thushag@users.noreply.github.com>
2024-11-13 00:33:11 +0530

    ContactUsRepository Dependancy Injection Created

--- MS3-Back-End/Repository/ContactUsRepository.cs
+++ MS3-Back-End/Repository/ContactUsRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace MS3_Back_End.Repository
+﻿using MS3_Back_End.DBContext;
+using MS3_Back_End.IRepository;
+
+namespace MS3_Back_End.Repository
 {
-    public class ContactUsRepository
+    public class ContactUsRepository: IContactUsRepository
     {
+        public readonly AppDBContext _dbContext;
+
+        public ContactUsRepository(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
     }
 }
