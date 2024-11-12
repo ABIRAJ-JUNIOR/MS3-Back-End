6269b93da61640c47d49aef903b1b55e673e830a
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-12 13:31:09 +0530

    iservce AddAuditLog done

--- MS3-Back-End/IService/IAuditLogService.cs
+++ MS3-Back-End/IService/IAuditLogService.cs
@@ -1,6 +1,11 @@
-﻿namespace MS3_Back_End.IService
+﻿using MS3_Back_End.DTOs.RequestDTOs.AuditLog;
+using MS3_Back_End.DTOs.ResponseDTOs.AuditLog;
+
+namespace MS3_Back_End.IService
 {
     public interface IAuditLogService
     {
+        Task<AuditLogResponceDTO> AddAuditLog(AuditLogRequestDTO auditLog);
+
     }
 }
