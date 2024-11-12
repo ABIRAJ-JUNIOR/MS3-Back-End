c14234dc326c88884bfc24a9e250362d5838af1e
Arshad-85 <mohammedarshath339@gmail.com>
2024-11-13 01:33:12 +0530

    ICourseCategoryService added, Post API Finished

--- MS3-Back-End/IService/ICourseCategoryService.cs
+++ MS3-Back-End/IService/ICourseCategoryService.cs
@@ -1,6 +1,10 @@
-﻿namespace MS3_Back_End.IService
+﻿using MS3_Back_End.DTOs.RequestDTOs.CourseCategory;
+using MS3_Back_End.DTOs.ResponseDTOs.CourseCategory;
+
+namespace MS3_Back_End.IService
 {
     public interface ICourseCategoryService
     {
+        Task<CourseCategoryResponseDTO> AddCategory(CourseCategoryRequestDTO courseCategoryRequestDTO);
     }
 }
