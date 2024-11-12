4bc5b403533a59cd343487354a892524acde14cc
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-13 01:39:09 +0530

    NotificationController created

---
+++ MS3-Back-End/Controllers/NotificationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MS3_Back_End.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+    }
+}
