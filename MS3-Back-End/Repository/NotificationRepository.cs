d2bb0ae3bfbfcfbff60e29aa882fc75e2d493afd
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-13 00:25:58 +0530

    dbcontext inject

--- MS3-Back-End/Repository/NotificationRepository.cs
+++ MS3-Back-End/Repository/NotificationRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace MS3_Back_End.Repository
+﻿using MS3_Back_End.DBContext;
+
+namespace MS3_Back_End.Repository
 {
     public class NotificationRepository
     {
+        private readonly AppDBContext _dbContext;
+
+        public NotificationRepository(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
     }
 }
