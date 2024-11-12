f0c1654d90fdde0426ed4332ccfc892eb3bd5730
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-12 15:08:28 +0530

    DeleteAuditlog  Iservice done

--- MS3-Back-End/IService/IAuditLogService.cs
+++ MS3-Back-End/IService/IAuditLogService.cs
@@ -13,6 +13,9 @@ namespace MS3_Back_End.IService
         Task<AuditLogResponceDTO> GetAuditLogByID(Guid id);
         Task<AuditLogResponceDTO> UpdateAuditLog(Guid auditlogid, AuditLogUpdateRequest auditLogService);
 
+        Task<AuditLogResponceDTO> DeleteAuditlog(Guid id);
+
+
 
 
 
