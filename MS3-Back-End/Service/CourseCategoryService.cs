c30e7c30c5ce28b581dc5558ca8f97382cdd81c0
Arshad-85 <mohammedarshath339@gmail.com>
2024-11-13 01:30:42 +0530

    Addcategory Service

--- MS3-Back-End/Service/CourseCategoryService.cs
+++ MS3-Back-End/Service/CourseCategoryService.cs
@@ -1,4 +1,7 @@
-﻿using MS3_Back_End.IRepository;
+﻿using MS3_Back_End.DTOs.RequestDTOs.CourseCategory;
+using MS3_Back_End.DTOs.ResponseDTOs.CourseCategory;
+using MS3_Back_End.Entities;
+using MS3_Back_End.IRepository;
 using MS3_Back_End.IService;
 
 namespace MS3_Back_End.Service
@@ -10,6 +13,25 @@ namespace MS3_Back_End.Service
         public CourseCategoryService(ICourseCategoryRepository courseCategoryRepository)
         {
             _courseCategoryRepository = courseCategoryRepository;
+        }
+
+        public async Task<CourseCategoryResponseDTO> AddCategory(CourseCategoryRequestDTO courseCategoryRequestDTO)
+        {
+            var Category = new CourseCategory
+            {
+                CategoryName = courseCategoryRequestDTO.CategoryName,
+                Description = courseCategoryRequestDTO.Description
+            };
+
+            var data = await _courseCategoryRepository.AddCategory(Category);
+
+            var CourseCategoryResponse = new CourseCategoryResponseDTO
+            {
+                Id = data.Id,
+                CategoryName = data.CategoryName,
+                Description = data.Description
+            };
+            return CourseCategoryResponse;
         }
 
     }
