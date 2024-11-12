c81b86db4eeef0ba307a176643e0f2c1ed1dd2fa
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-12 14:21:21 +0530

    IRepo GetAuditLogByID done

--- MS3-Back-End/IRepository/IAuditLogRepository.cs
+++ MS3-Back-End/IRepository/IAuditLogRepository.cs
@@ -8,6 +8,9 @@ namespace MS3_Back_End.IRepository
         Task<AuditLog> AddAuditLog(AuditLog auditLog);
         Task<List<AuditLog>> GetAllAuditlogs();
         Task<List<AuditLog>> GetAuditLogsbyAdminId(Guid id);
+        Task<AuditLog> GetAuditLogByID(Guid id);
+
+
 
 
 
