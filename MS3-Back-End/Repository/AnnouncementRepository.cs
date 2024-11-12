94c774ffb7a1c32d956e611e1fd07134851ede8f
UT01520 <UT01520@UNICOMTIC15>
2024-11-12 15:11:38 +0530

    Add AddAnouncement in repo

--- MS3-Back-End/Repository/AnnouncementRepository.cs
+++ MS3-Back-End/Repository/AnnouncementRepository.cs
@@ -1,4 +1,6 @@
-﻿using MS3_Back_End.DBContext;
+﻿using Microsoft.EntityFrameworkCore;
+using MS3_Back_End.DBContext;
+using MS3_Back_End.Entities;
 
 namespace MS3_Back_End.Repository
 {
@@ -9,5 +11,25 @@ namespace MS3_Back_End.Repository
         {
             _Db = db;
         }
+
+        public async Task<Announcement> AddAnnouncement(Announcement AnouncementReq)
+        {
+            var Data = await _Db.Announcements.SingleOrDefaultAsync(n => n.Title==AnouncementReq.Title);
+            if (Data == null)
+            {
+                var data = await _Db.Announcements.AddAsync(AnouncementReq);
+                await _Db.SaveChangesAsync();
+                return data.Entity;
+            }
+            else
+            {
+                throw new Exception("Your Course Already Added");
+            }
+
+        }
+
+
+
+
     }
 }
