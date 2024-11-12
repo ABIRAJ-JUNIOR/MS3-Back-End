e00281614af986dc097c33609fbd81e8e5d9af51
Arshad-85 <mohammedarshath339@gmail.com>
2024-11-13 01:26:45 +0530

    CourseCategoryResponseDTO fields added

--- MS3-Back-End/DTOs/ResponseDTOs/CourseCategory/CourseCategoryResponseDTO.cs
+++ MS3-Back-End/DTOs/ResponseDTOs/CourseCategory/CourseCategoryResponseDTO.cs
@@ -2,5 +2,9 @@
 {
     public class CourseCategoryResponseDTO
     {
+        public Guid Id { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
     }
 }
