72adddb312f87de1d8cef97308c5dc31d476fb6c
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-13 00:28:08 +0530

    INotificationRepository created

--- MS3-Back-End/Repository/NotificationRepository.cs
+++ MS3-Back-End/Repository/NotificationRepository.cs
@@ -1,8 +1,9 @@
 ﻿using MS3_Back_End.DBContext;
+using MS3_Back_End.IRepository;
 
 namespace MS3_Back_End.Repository
 {
-    public class NotificationRepository
+    public class NotificationRepository: INotificationRepository
     {
         private readonly AppDBContext _dbContext;
 
