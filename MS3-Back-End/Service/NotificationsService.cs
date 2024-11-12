393addc3d2518d2028cb7d60ebe17ec98df3a2ea
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-13 00:34:41 +0530

    INotificationRepository inject

---
+++ MS3-Back-End/Service/NotificationsService.cs
@@ -0,0 +1,14 @@
+﻿using MS3_Back_End.IRepository;
+
+namespace MS3_Back_End.Service
+{
+    public class NotificationsService
+    {
+        private readonly INotificationRepository _notificationRepository;
+
+        public NotificationsService(INotificationRepository notificationRepository)
+        {
+            _notificationRepository = notificationRepository;
+        }
+    }
+}
