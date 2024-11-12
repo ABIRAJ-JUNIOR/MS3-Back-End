294349b33f4e9856976150189b30ba06678675ec
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-13 01:42:18 +0530

    INotificationsService inject

--- MS3-Back-End/Controllers/NotificationController.cs
+++ MS3-Back-End/Controllers/NotificationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MS3_Back_End.IService;
 
 namespace MS3_Back_End.Controllers
 {
@@ -7,5 +8,11 @@ namespace MS3_Back_End.Controllers
     [ApiController]
     public class NotificationController : ControllerBase
     {
+        private readonly INotificationsService _notificationsService;
+
+        public NotificationController(INotificationsService notificationsService)
+        {
+            _notificationsService = notificationsService;
+        }
     }
 }
