9e857892395fa2542b058dd647a75210b55239b8
Arshad-85 <mohammedarshath339@gmail.com>
2024-11-13 01:26:12 +0530

    CourseCategoryRequestDTO Fields added

--- MS3-Back-End/DTOs/RequestDTOs/CourseCategory/CourseCategoryRequestDTO.cs
+++ MS3-Back-End/DTOs/RequestDTOs/CourseCategory/CourseCategoryRequestDTO.cs
@@ -2,5 +2,9 @@
 {
     public class CourseCategoryRequestDTO
     {
+        public Guid Id { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
     }
 }
