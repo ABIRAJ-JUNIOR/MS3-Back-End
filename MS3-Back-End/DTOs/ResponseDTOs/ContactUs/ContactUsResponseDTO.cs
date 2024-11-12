4bcf84de10bc88b954af65f445241184aebd93ec
thushag <141636675+thushag@users.noreply.github.com>
2024-11-13 00:39:13 +0530

    ContactUsResponseDTO Created

--- MS3-Back-End/DTOs/ResponseDTOs/ContactUs/ContactUsResponseDTO.cs
+++ MS3-Back-End/DTOs/ResponseDTOs/ContactUs/ContactUsResponseDTO.cs
@@ -2,5 +2,13 @@
 {
     public class ContactUsResponseDTO
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public string Response { get; set; } = string.Empty;
+        public DateOnly DateSubmited { get; set; }
+        public bool IsRead { get; set; }
+
     }
 }
