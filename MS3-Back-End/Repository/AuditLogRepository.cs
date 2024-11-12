41ccb32df30837401bb1f700032b1ddd55200e49
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-12 12:45:47 +0530

    dbcontext inject

--- MS3-Back-End/Repository/AuditLogRepository.cs
+++ MS3-Back-End/Repository/AuditLogRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace MS3_Back_End.Repository
+﻿using MS3_Back_End.DBContext;
+using MS3_Back_End.IRepository;
+
+namespace MS3_Back_End.Repository
 {
-    public class AuditLogRepository
+    public class AuditLogRepository: IAuditLogRepository
     {
+        private readonly AppDBContext _dbContext;
+
+        public AuditLogRepository(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
     }
 }
