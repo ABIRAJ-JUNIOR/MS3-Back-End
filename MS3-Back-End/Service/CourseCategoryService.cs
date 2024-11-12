9d240df813a5a802d9b35188fe57108cecaa2ddb
Arshad-85 <mohammedarshath339@gmail.com>
2024-11-13 01:28:29 +0530

    CourseCategoryService dependency injection cretaed

--- MS3-Back-End/Service/CourseCategoryService.cs
+++ MS3-Back-End/Service/CourseCategoryService.cs
@@ -1,6 +1,16 @@
-﻿namespace MS3_Back_End.Service
+﻿using MS3_Back_End.IRepository;
+using MS3_Back_End.IService;
+
+namespace MS3_Back_End.Service
 {
-    public class CourseCategoryService
+    public class CourseCategoryService: ICourseCategoryService
     {
+        private readonly ICourseCategoryRepository _courseCategoryRepository;
+
+        public CourseCategoryService(ICourseCategoryRepository courseCategoryRepository)
+        {
+            _courseCategoryRepository = courseCategoryRepository;
+        }
+
     }
 }
