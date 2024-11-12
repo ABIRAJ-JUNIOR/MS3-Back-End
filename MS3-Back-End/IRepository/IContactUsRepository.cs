73cca9ac8159968586c2d7e48401ca12b2fef4f2
thushag <141636675+thushag@users.noreply.github.com>
2024-11-13 00:42:43 +0530

    IContactUsRepository Created

--- MS3-Back-End/IRepository/IContactUsRepository.cs
+++ MS3-Back-End/IRepository/IContactUsRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace MS3_Back_End.IRepository
+﻿using MS3_Back_End.Entities;
+
+namespace MS3_Back_End.IRepository
 {
     public interface IContactUsRepository
     {
+        Task<ContactUs> AddMessage(ContactUs contactUs);
     }
 }
