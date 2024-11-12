5cb91bae50182cea4147841a6a6d9af40714aa88
thushag <141636675+thushag@users.noreply.github.com>
2024-11-13 00:34:08 +0530

    ContactUs Service Dependancy Injection Created

--- MS3-Back-End/Service/ContactUsService.cs
+++ MS3-Back-End/Service/ContactUsService.cs
@@ -1,6 +1,15 @@
-﻿namespace MS3_Back_End.Service
+﻿using MS3_Back_End.IRepository;
+using MS3_Back_End.IService;
+
+namespace MS3_Back_End.Service
 {
-    public class ContactUsService
+    public class ContactUsService: IContactUsService
     {
+        public readonly IContactUsRepository _contactUsRepository;
+
+        public ContactUsService(IContactUsRepository contactUsRepository)
+        {
+            _contactUsRepository = contactUsRepository;
+        }
     }
 }
