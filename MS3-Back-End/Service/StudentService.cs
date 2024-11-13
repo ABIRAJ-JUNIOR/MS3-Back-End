78e596d65969be54e065d552dc90dfc7c7c68b32
Anojan1520 <unicomtic1520@gmail.com>
2024-11-13 11:46:55 +0530

    add addsrtudent struct

--- MS3-Back-End/Service/StudentService.cs
+++ MS3-Back-End/Service/StudentService.cs
@@ -1,6 +1,12 @@
 ﻿using MS3_Back_End.DBContext;
+using MS3_Back_End.DTOs.RequestDTOs.Course;
+using MS3_Back_End.DTOs.RequestDTOs.Student;
+using MS3_Back_End.DTOs.ResponseDTOs.Course;
+using MS3_Back_End.DTOs.ResponseDTOs.Student;
+using MS3_Back_End.Entities;
 using MS3_Back_End.IRepository;
 using MS3_Back_End.IService;
+using MS3_Back_End.Repository;
 
 namespace MS3_Back_End.Service
 {
@@ -11,6 +17,27 @@ namespace MS3_Back_End.Service
         {
             _StudentRepo = StudentRepo;
         }
+
+        public async Task<StudentResponseDTO> AddStudent(StudentRequestDTO Stundent)
+        {
+
+            var Student = new Student
+            {
+               
+            };
+
+            var data = await _StudentRepo.AddStudent(Student);
+
+            var StudentReponse = new StudentResponseDTO
+            {
+               
+            };
+
+            return StudentReponse;
+
+        }
+
+
 
     }
 }
