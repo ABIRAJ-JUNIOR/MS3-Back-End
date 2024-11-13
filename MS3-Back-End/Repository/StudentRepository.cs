8d6c9d02d07eba7f8c3cc8d833a2d8a4f0382a37
Anojan1520 <unicomtic1520@gmail.com>
2024-11-13 11:35:07 +0530

    implemnt AlllStudents

--- MS3-Back-End/Repository/StudentRepository.cs
+++ MS3-Back-End/Repository/StudentRepository.cs
@@ -14,7 +14,7 @@ namespace MS3_Back_End.Repository
         }
         public async Task<Student> AddStudent(Student StudentReq)
         {
-            var student = await _Db.Students.SingleOrDefaultAsync(s=>s.Nic== StudentReq.Nic);
+            var student = await _Db.Students.SingleOrDefaultAsync(s => s.Nic == StudentReq.Nic);
             if (student == null)
             {
                 var data = await _Db.Students.AddAsync(StudentReq);
@@ -31,10 +31,24 @@ namespace MS3_Back_End.Repository
 
         public async Task<List<Student>> SearchStudent(string SearchText)
         {
-            var data = await _Db.Students.Where(n => n.FirstName.Contains(SearchText) || n.LastName.Contains(SearchText) || n.Nic.Contains(SearchText).ToListAsync();
+            var data = await _Db.Students .Where(n => n.FirstName.Contains(SearchText) || 
+               n.LastName.Contains(SearchText) ||
+               n.Nic.Contains(SearchText)).Include(a => a.Address).ToListAsync();
             return data;
         }
 
+
+        public async Task<List<Student>> GetAllStudente()
+        {
+            var data = await _Db.Students.Where(c => c.IsActive == true).Include(a => a.Address).ToListAsync();
+            return data;
+        }
+        public async Task<Course> GetAllSrudentById(Guid CourseId)
+        {
+            var data = await _Db.Courses.SingleOrDefaultAsync(c => c.Id == CourseId && c.IsDeleted == false);
+            return data;
+        }
+       
 
 
 
