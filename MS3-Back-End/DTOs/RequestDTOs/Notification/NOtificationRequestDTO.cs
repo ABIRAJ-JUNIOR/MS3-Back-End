444565e1dc73cd44d4e31aa4a4866c0c84b09c5c
Arshad-85 <mohammedarshath339@gmail.com>
2024-11-14 00:12:42 +0530

    NOtificationRequestDTO updated

--- MS3-Back-End/DTOs/RequestDTOs/Notification/NOtificationRequestDTO.cs
+++ MS3-Back-End/DTOs/RequestDTOs/Notification/NOtificationRequestDTO.cs
@@ -4,8 +4,12 @@ namespace MS3_Back_End.DTOs.RequestDTOs.Notification
 {
     public class NOtificationRequestDTO
     {
+        public Guid Id { get; set; }
         public string Message { get; set; } = string.Empty;
         public DateOnly DateSent { get; set; }
         public NotificationType NotificationType { get; set; }
+        public bool IsRead { get; set; }
+
+        public Guid StudentId { get; set; }
     }
 }
