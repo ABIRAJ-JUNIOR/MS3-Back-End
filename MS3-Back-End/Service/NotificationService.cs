2018534afdfea9045a363f7a8c92591551db7bf5
Arshad-85 <mohammedarshath339@gmail.com>
2024-11-13 23:24:32 +0530

    Notification Service dependency injection created

--- MS3-Back-End/Service/NotificationService.cs
+++ MS3-Back-End/Service/NotificationService.cs
@@ -1,6 +1,15 @@
-﻿namespace MS3_Back_End.Service
+﻿using MS3_Back_End.IRepository;
+using MS3_Back_End.IService;
+
+namespace MS3_Back_End.Service
 {
-    public class NotificationService
+    public class NotificationService: INotificationService
     {
+        private readonly INotificationRepository _notificationRepository;
+
+        public NotificationService(INotificationRepository notificationRepository)
+        {
+            _notificationRepository = notificationRepository;
+        }
     }
 }
