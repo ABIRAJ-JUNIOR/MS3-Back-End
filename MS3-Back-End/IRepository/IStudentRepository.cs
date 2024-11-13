65bac69dfbb64b9825e139d626268ec068253f87
Anojan1520 <unicomtic1520@gmail.com>
2024-11-13 11:42:23 +0530

    implement interface in repo

--- MS3-Back-End/IRepository/IStudentRepository.cs
+++ MS3-Back-End/IRepository/IStudentRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace MS3_Back_End.IRepository
+﻿using MS3_Back_End.Entities;
+
+namespace MS3_Back_End.IRepository
 {
     public interface IStudentRepository
     {
+        Task<Student> AddStudent(Student StudentReq);
+        Task<List<Student>> SearchStudent(string SearchText);
+        Task<List<Student>> GetAllStudente();
+        Task<Student> GetEnrollmentById(Guid StudentId);
+        Task<Student> UpdateStudent(Student Students);
+
     }
 }
