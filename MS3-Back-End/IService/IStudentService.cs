10419e6e9b3a4625fd2a713209f84432f5b72e0b
Anojan1520 <unicomtic1520@gmail.com>
2024-11-13 12:43:52 +0530

    implement interface

--- MS3-Back-End/IService/IStudentService.cs
+++ MS3-Back-End/IService/IStudentService.cs
@@ -6,6 +6,11 @@ namespace MS3_Back_End.IService
 {
     public interface IStudentService
     {
-      
+        Task<StudentResponseDTO> AddStudent(StudentRequestDTO StudentReq);
+        Task<List<StudentResponseDTO>> SearchStudent(string SearchText);
+        Task<StudentResponseDTO> GetStudentById(Guid StudentId);
+        Task<List<StudentResponseDTO>> GetAllStudent();
+        Task<StudentResponseDTO> UpdateStudent(StudentUpdateDTO studentUpdate);
+        Task<string> DeleteStudent(Guid Id);
     }
 }
