ab50f970b1459f4c0e4cdf1dc176f1c1b58a1130
Anojan1520 <unicomtic1520@gmail.com>
2024-11-13 11:18:33 +0530

    inject dependacny

--- MS3-Back-End/Service/StudentService.cs
+++ MS3-Back-End/Service/StudentService.cs
@@ -1,6 +1,13 @@
-﻿namespace MS3_Back_End.Service
+﻿using MS3_Back_End.DBContext;
+
+namespace MS3_Back_End.Service
 {
     public class StudentService
     {
+        private readonly AppDBContext _db;
+        public StudentService(AppDBContext db)
+        {
+            _db = db;
+        }
     }
 }
