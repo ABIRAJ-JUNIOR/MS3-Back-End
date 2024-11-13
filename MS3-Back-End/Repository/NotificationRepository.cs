1ab1491485c7ed2dcd65d44916b76fe5e7435192
Arshad-85 <mohammedarshath339@gmail.com>
2024-11-13 23:34:51 +0530

    NotificationRepository AddNotification Created

--- MS3-Back-End/Repository/NotificationRepository.cs
+++ MS3-Back-End/Repository/NotificationRepository.cs
@@ -1,4 +1,6 @@
-﻿using MS3_Back_End.DBContext;
+﻿using Microsoft.EntityFrameworkCore;
+using MS3_Back_End.DBContext;
+using MS3_Back_End.Entities;
 using MS3_Back_End.IRepository;
 
 namespace MS3_Back_End.Repository
@@ -10,6 +12,13 @@ namespace MS3_Back_End.Repository
         public NotificationRepository(AppDBContext _appDBContext)
         {
             appDBContext = _appDBContext;
+        }
+
+        public async Task<Notification> AddNotification(Notification _notification)
+        {
+            var notification = await appDBContext.Notifications.AddAsync(_notification);
+            await appDBContext.SaveChangesAsync();
+            return notification.Entity;
         }
     }
 }
