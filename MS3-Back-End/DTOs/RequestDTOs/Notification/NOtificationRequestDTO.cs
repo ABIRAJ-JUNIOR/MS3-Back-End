055f3009efe07ca1fc920af71a95fb3b5b654b66
Arshad-85 <mohammedarshath339@gmail.com>
2024-11-13 23:29:01 +0530

    NotificationRequestDTO fields added

--- MS3-Back-End/DTOs/RequestDTOs/Notification/NOtificationRequestDTO.cs
+++ MS3-Back-End/DTOs/RequestDTOs/Notification/NOtificationRequestDTO.cs
@@ -1,6 +1,11 @@
-﻿namespace MS3_Back_End.DTOs.RequestDTOs.Notification
+﻿using MS3_Back_End.Entities;
+
+namespace MS3_Back_End.DTOs.RequestDTOs.Notification
 {
     public class NOtificationRequestDTO
     {
+        public string Message { get; set; } = string.Empty;
+        public DateOnly DateSent { get; set; }
+        public NotificationType NotificationType { get; set; }
     }
 }
