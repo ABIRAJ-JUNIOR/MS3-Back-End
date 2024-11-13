3455f8bf6bb603ffd40b484ffc9969aa6bdecbb3
Arshad-85 <mohammedarshath339@gmail.com>
2024-11-13 23:22:54 +0530

    Notification Repo dependency injection

--- MS3-Back-End/Repository/NotificationRepository.cs
+++ MS3-Back-End/Repository/NotificationRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace MS3_Back_End.Repository
+﻿using MS3_Back_End.DBContext;
+using MS3_Back_End.IRepository;
+
+namespace MS3_Back_End.Repository
 {
-    public class NotificationRepository
+    public class NotificationRepository: INotificationRepository
     {
+        private readonly AppDBContext appDBContext;
+
+        public NotificationRepository(AppDBContext _appDBContext)
+        {
+            appDBContext = _appDBContext;
+        }
     }
 }
