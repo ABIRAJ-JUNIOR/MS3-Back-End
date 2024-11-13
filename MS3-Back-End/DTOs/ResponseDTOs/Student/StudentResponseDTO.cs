ee90353e5c7956cf86745b7ce6909df207f1a7a1
Anojan1520 <unicomtic1520@gmail.com>
2024-11-13 11:16:41 +0530

    Update Student Response RequestDTo and UpdateDto

---
+++ MS3-Back-End/DTOs/ResponseDTOs/Student/StudentResponseDTO.cs
@@ -0,0 +1,22 @@
+﻿using MS3_Back_End.Entities;
+
+namespace MS3_Back_End.DTOs.ResponseDTOs.Student
+{
+    public class StudentResponseDTO
+    {
+        public Guid Id { get; set; }
+        public string Nic { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string? LastName { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; } = DateTime.MinValue;
+        public Gender Gender { get; set; }
+        public string Phone { get; set; } = string.Empty;
+        public string ImagePath { get; set; } = string.Empty;
+        public DateTime CteatedDate { get; set; } = DateTime.MinValue;
+        public DateTime? UpdatedDate { get; set; } = DateTime.MinValue;
+        public bool IsActive { get; set; } = true;
+
+
+
+    }
+}
