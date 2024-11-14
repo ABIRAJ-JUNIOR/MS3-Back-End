9f3954236dac05ac2c40e08da305fcd0e9aa8595
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-11-14 23:03:13 +0530

    Modified ContactUs Request DTO

--- MS3-Back-End/DTOs/RequestDTOs/ContactUs/ContactUsRequestDTO.cs
+++ MS3-Back-End/DTOs/RequestDTOs/ContactUs/ContactUsRequestDTO.cs
@@ -5,8 +5,6 @@
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Message { get; set; } = string.Empty;
-        public DateOnly DateSubmited { get; set; }
-        public bool IsRead { get; set; }
 
     }
 }
