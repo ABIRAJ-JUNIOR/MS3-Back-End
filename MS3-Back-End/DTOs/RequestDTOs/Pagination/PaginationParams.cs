79a9093fe4fd7d91105938e1b66a8b7f627c2051
Anojan1520 <unicomtic1520@gmail.com>
2024-11-15 12:00:21 +0530

    implement pagination method in repo student

---
+++ MS3-Back-End/DTOs/RequestDTOs/Pagination/PaginationParams.cs
@@ -0,0 +1,8 @@
+﻿namespace MS3_Back_End.DTOs.RequestDTOs.Pagination
+{
+    public class PaginationParams
+    {
+        public int PageIndex { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
