35a7710111478bf91cef7fc5d65a781604d0a41e
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-17 11:59:39 +0530

    IFeedbacksService Add function done

--- MS3-Back-End/IService/IFeedbacksService.cs
+++ MS3-Back-End/IService/IFeedbacksService.cs
@@ -1,6 +1,11 @@
-﻿namespace MS3_Back_End.IService
+﻿using MS3_Back_End.DTOs.RequestDTOs.Feedbacks;
+using MS3_Back_End.DTOs.ResponseDTOs;
+
+namespace MS3_Back_End.IService
 {
     public interface IFeedbacksService
     {
+        Task<FeedbacksResponceDTO> AddFeedbacks(FeedbacksRequestDTO reqfeedback);
+
     }
 }
