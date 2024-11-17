bc085c34598a69589bf764000c3be6d6a3ebc550
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-17 12:01:55 +0530

    FeedbacksController

---
+++ MS3-Back-End/Controllers/FeedbacksController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MS3_Back_End.IService;
+
+namespace MS3_Back_End.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedbacksController : ControllerBase
+    {
+        private readonly IFeedbacksService feedbackService;
+
+        public FeedbacksController(IFeedbacksService feedbackService)
+        {
+            this.feedbackService = feedbackService;
+        }
+        [HttpPost]
+    }
+}
