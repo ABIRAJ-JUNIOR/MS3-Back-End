7dadb0c242080c671ceec474538a412dac42ae98
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-17 12:04:47 +0530

    AddFeedback controler done

--- MS3-Back-End/Controllers/FeedbacksController.cs
+++ MS3-Back-End/Controllers/FeedbacksController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using MS3_Back_End.DTOs.RequestDTOs.Feedbacks;
 using MS3_Back_End.IService;
 
 namespace MS3_Back_End.Controllers
@@ -8,12 +10,17 @@ namespace MS3_Back_End.Controllers
     [ApiController]
     public class FeedbacksController : ControllerBase
     {
-        private readonly IFeedbacksService feedbackService;
+        private readonly IFeedbacksService _feedbackService;
 
         public FeedbacksController(IFeedbacksService feedbackService)
         {
-            this.feedbackService = feedbackService;
+            _feedbackService = feedbackService;
         }
         [HttpPost]
+        public async Task<IActionResult> AddFeedback(FeedbacksRequestDTO feedbacksRequestDTO) 
+        {
+            var data=await _feedbackService.AddFeedbacks(feedbacksRequestDTO);
+            return Ok(data);
+        }
     }
 }
