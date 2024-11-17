bdaea78de0150a03d7172e8baded87680ac9c943
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-11-17 11:09:30 +0530

    FeedbacksRepository created

---
+++ MS3-Back-End/Repository/FeedbacksRepository.cs
@@ -0,0 +1,15 @@
+﻿using MS3_Back_End.DBContext;
+
+namespace MS3_Back_End.Repository
+{
+    public class FeedbacksRepository
+    {
+        private readonly AppDBContext _dbContext;
+
+        public FeedbacksRepository(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+    
+}
