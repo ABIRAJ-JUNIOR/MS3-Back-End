c1ae59938af0b4f8f3282d212373ad185de54064
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-11-19 11:06:57 +0530

    implement Authorization

--- MS3-Back-End/Controllers/StudentController.cs
+++ MS3-Back-End/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MS3_Back_End.DTOs.Image;
 using MS3_Back_End.DTOs.Pagination;
