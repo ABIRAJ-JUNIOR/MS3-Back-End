2ec6935336ff15834359dd936a96fed1cf27bbc7
Anojan1520 <unicomtic1520@gmail.com>
2024-12-06 22:37:08 +0530

    add password change modal in modal Component

---
+++ MS3-Back-End/DTOs/RequestDTOs/Password/paswordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MS3_Back_End.DTOs.RequestDTOs.Password
+{
+    public class paswordRequest
+    {
+            public string OldPassword { get; set; }
+            public string ConfirmPassword { get; set; }
+
+    }
+}
