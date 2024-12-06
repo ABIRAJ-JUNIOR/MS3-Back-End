2543fce043ad19361085d7bb4d7d85307e21edac
Arshad-85 <mohammedarshath339@gmail.com>
2024-12-06 13:59:49 +0530

    Payment OverView Ongoing

---
+++ MS3-Back-End/DTOs/ResponseDTOs/Payment/PaymentOverview.cs
@@ -0,0 +1,10 @@
+﻿namespace MS3_Back_End.DTOs.ResponseDTOs.Payment
+{
+    public class PaymentOverview
+    {
+        public double TotalAmount { get; set; }
+        public double FullPayment {  get; set; }
+        public double Installment {  get; set; }
+        public double OverDue {  get; set; }
+    }
+}
--- MS3-Back-End/Repository/PaymentRepository.cs
+++ MS3-Back-End/Repository/PaymentRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MS3_Back_End.DBContext;
+using MS3_Back_End.DTOs.ResponseDTOs.Payment;
 using MS3_Back_End.Entities;
 using MS3_Back_End.IRepository;
 
@@ -32,5 +33,26 @@ namespace MS3_Back_End.Repository
             var recentPayments = await _context.Payments.OrderByDescending(p => p.PaymentDate).Take(5).ToListAsync();
             return recentPayments;
         }
+
+        //public async Task<PaymentOverview> GetPaymentOverview()
+        //{
+        //    var paymentOverview = await (from payment in _context.Payments
+        //                                 join enrollment in _context.Enrollments
+        //                                     on payment.EnrollmentId equals enrollment.Id into enrollmentGroup
+        //                                 from enrollment in enrollmentGroup.DefaultIfEmpty()
+        //                                 join courseSchedule in _context.CourseSchedules
+        //                                     on enrollment.CourseScheduleId equals courseSchedule.Id into courseScheduleGroup
+        //                                 from courseSchedule in courseScheduleGroup.DefaultIfEmpty()
+        //                                 join course in _context.Courses
+        //                                     on courseSchedule.CourseId equals course.Id into courseGroup
+        //                                 from course in courseGroup.DefaultIfEmpty()
+        //                                 select new PaymentOverview
+        //                                 {
+                                            
+        //                                 }).ToListAsync();
+
+        //    return paymentOverview;
+        //}
+
     }
 }
