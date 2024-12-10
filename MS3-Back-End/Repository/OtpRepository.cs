f01b0a57905f69c0a77c3dd7354461ed7787163a
Anojan1520 <unicomtic1520@gmail.com>
2024-12-10 22:38:02 +0530

    implement email verification method in repository

--- MS3-Back-End/Repository/OtpRepository.cs
+++ MS3-Back-End/Repository/OtpRepository.cs
@@ -1,5 +1,8 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
 using MS3_Back_End.DBContext;
+using MS3_Back_End.DTOs.Otp;
+using MS3_Back_End.Entities;
 
 namespace MS3_Back_End.Repository
 {
@@ -11,7 +14,20 @@ namespace MS3_Back_End.Repository
             _Db = db;
         }
 
-      
+        public async Task<bool> emailVerification(GenerateOtp otpDetail)
+        {
+            var responseData = await _Db.Users.SingleOrDefaultAsync(user=>user.Email==otpDetail.Email);
+            if (responseData != null)
+            {
+                return true;
+            }
+            else
+            {
+                throw new Exception("Email Not Valid");
+            }
+
+        }
+
 
     }
 }
